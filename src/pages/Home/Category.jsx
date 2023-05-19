ad9c86873b93b7e42a50d00c112dc1258c03cbc6
Mynul Islam <mynul.sakil@gmail.com>
2023-05-19 22:10:58 +0600

    adding 404 page

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -11,34 +11,36 @@ const Category = () => {
     }, [])
     return (
         <div className='my-20 shadow-md rounded-lg md:p-4'>
+            <Loading>
                 <h2 className='text-4xl text-pink-400 text-center mb-10 font-bold'>Product Category You may like</h2>
 
-            <Tabs>
-                <TabList>
-                    <Tab>Anime</Tab>
-                    <Tab>Marvel</Tab>
-                    <Tab>Transformers</Tab>
-                </TabList>
+                <Tabs>
+                    <TabList>
+                        <Tab>Anime</Tab>
+                        <Tab>Marvel</Tab>
+                        <Tab>Transformers</Tab>
+                    </TabList>
 
-                <TabPanel>
-                   <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4'>
+                    <TabPanel>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4'>
 
-                    {toys.map(toy => toy?.category === "Anime" && <CategoryCard key={toy._id} toy={toy} />)
-                    }
-                   </div>
-                </TabPanel>
-                <TabPanel>
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4'>
-                    {toys.map(toy => toy?.category === "Marvel" && <CategoryCard key={toy._id} toy={toy} />)
-                    }
-                    </div>
-                </TabPanel> <TabPanel>
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4 '>
-                    {toys.map(toy => toy?.category === "Transformers" && <CategoryCard key={toy._id} toy={toy} />)
-                    }
-                    </div>
-                </TabPanel>
-            </Tabs>
+                            {toys.map(toy => toy?.category === "Anime" && <CategoryCard key={toy._id} toy={toy} />)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4'>
+                            {toys.map(toy => toy?.category === "Marvel" && <CategoryCard key={toy._id} toy={toy} />)
+                            }
+                        </div>
+                    </TabPanel> <TabPanel>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 p-4 '>
+                            {toys.map(toy => toy?.category === "Transformers" && <CategoryCard key={toy._id} toy={toy} />)
+                            }
+                        </div>
+                    </TabPanel>
+                </Tabs>
+            </Loading>
         </div>
     );
 
